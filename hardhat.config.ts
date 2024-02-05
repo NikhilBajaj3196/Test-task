8bad85785a1478f67ec7c69d49e172cfb6a5612c
Nikhil Bajaj <nikbajaj3196@gmail.com>
2024-02-05 22:11:36 +0530

    feat: updated repo configs

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,9 +1,12 @@
-require("@nomicfoundation/hardhat-toolbox");
-require('@typechain/hardhat')
-require('@nomicfoundation/hardhat-ethers')
-require('@nomicfoundation/hardhat-chai-matchers')
+import "@nomicfoundation/hardhat-toolbox";
+import "@typechain/hardhat";
+import "@nomicfoundation/hardhat-ethers";
+import "@nomicfoundation/hardhat-chai-matchers";
+import * as dotenv from "dotenv";
 
-/** @type import('hardhat/config').HardhatUserConfig */
+dotenv.config();
+const DEPLOYER_PRIVATE_KEY = process.env.DEPLOYER_PRIVATE_KEY;
+
 module.exports = {
   solidity: "0.8.20",
   compilerOptions: {
@@ -14,6 +17,45 @@ module.exports = {
     "outDir": "dist",
     "resolveJsonModule": true
   },
-  include: ["./scripts", "./test", "./typechain-types"],
-  files: ["./hardhat.config.ts"]
+  include: ["./scripts", "./test", "./typechain"],
+  files: ["./hardhat.config.ts"],
+  networks: {
+    hardhat: isFork(),
+    bsctestnet: {
+      url: process.env.ARCHIVE_NODE_bsctestnet || "https://data-seed-prebsc-1-s1.binance.org:8545",
+      chainId: 97,
+      live: true,
+      gasPrice: 20000000000,
+      accounts: DEPLOYER_PRIVATE_KEY ? [`0x${DEPLOYER_PRIVATE_KEY}`] : [],
+    }
+  },
+  etherscan: {
+    apiKey: {
+      bscTestnet: process.env.ETHERSCAN_API_KEY || "ETHERSCAN_API_KEY",
+    }
+  },
+  accounts: DEPLOYER_PRIVATE_KEY ? [`0x${DEPLOYER_PRIVATE_KEY}`] : [],
 };
+
+function isFork() {
+  return process.env.FORK === "true"
+    ? {
+        allowUnlimitedContractSize: false,
+        loggingEnabled: false,
+        forking: {
+          url:
+            process.env[`ARCHIVE_NODE_${process.env.FORKED_NETWORK}`] ||
+            "https://data-seed-prebsc-1-s1.binance.org:8545",
+          blockNumber: 26349263,
+        },
+        accounts: {
+          accountsBalance: "1000000000000000000",
+        },
+        live: false,
+      }
+    : {
+        allowUnlimitedContractSize: true,
+        loggingEnabled: false,
+        live: false,
+      };
+}
