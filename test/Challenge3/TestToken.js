e0b82e51a0222bd1f13c85ff009b691736cd6770
Nikhil Bajaj <nikbajaj3196@gmail.com>
2024-02-05 22:09:52 +0530

    feat: challenge3 script

--- test/Challenge3/TestToken.js
+++ test/Challenge3/TestToken.js
@@ -1,40 +1,22 @@
-const Web3 = require('web3');
+const {Web3}  = require('web3');
+
+// ERC20 token contract ABI
+const contractABI = require("./TokenABI.json");
 
 // Set up BSC Testnet endpoint
 const bscTestnetEndpoint = 'https://data-seed-prebsc-1-s1.binance.org:8545';
 const web3 = new Web3(new Web3.providers.HttpProvider(bscTestnetEndpoint));
 
-// Replace the following with the actual contract address and ABI
-const contractAddress = '0x0000000000000000000000000000000000000000';
-const contractABI = [
-  // Replace with the actual ABI
-  {
-    "constant": true,
-    "inputs": [],
-    "name": "totalSupply",
-    "outputs": [{"name": "", "type": "uint256"}],
-    "payable": false,
-    "stateMutability": "view",
-    "type": "function"
-  },
-  {
-    "constant": false,
-    "inputs": [{"name": "_to", "type": "address"}, {"name": "_amount", "type": "uint256"}],
-    "name": "mint",
-    "outputs": [],
-    "payable": false,
-    "stateMutability": "nonpayable",
-    "type": "function"
-  }
-];
+// Contract deployed at bsc testnet
+const contractAddress = '0x9592C6C52A0db42374493869BFeE56DCe2893179';
 
 // Set up the contract instance
-const contract = new web3.eth.Contract(contractABI, contractAddress);
+const token = new web3.eth.Contract(contractABI, contractAddress);
 
 // Function to query total supply
 async function getTotalSupply() {
   try {
-    const totalSupply = await contract.methods.totalSupply().call();
+    const totalSupply = await token.methods.totalSupply().call();
     console.log('Total Supply:', totalSupply);
   } catch (error) {
     console.error('Error querying total supply:', error);
@@ -45,7 +27,7 @@ async function getTotalSupply() {
 async function mintTokens(toAddress, amount) {
   try {
     const accounts = await web3.eth.getAccounts();
-    const result = await contract.methods.mint(toAddress, amount).send({ from: accounts[0] });
+    const result = await token.methods.mint(toAddress, amount).send({ from: accounts[0] });
     console.log('Minting successful. Transaction Hash:', result.transactionHash);
   } catch (error) {
     console.error('Error minting tokens:', error);
@@ -54,4 +36,4 @@ async function mintTokens(toAddress, amount) {
 
 // Example usage
 getTotalSupply();
-mintTokens('0xReceiverAddress', '1000'); // Replace '0xReceiverAddress' with the actual BSC address
+mintTokens('0xReceiverAddress', '1000'); // Replace '0xReceiverAddress' with the actual BSC testnet address
